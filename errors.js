dbb7df37bbf8627d25cb83f9908dd1f9de7eca92
Harshvardhan <harshvardhan2672@gmail.com>
2025-09-27 10:58:05 +0530

    errors in js

---
+++ errors.js
@@ -0,0 +1,16 @@
+//syntax errors 
+let a=10;
+console.log(a);
+let a=20 
+// redeclaration of a
+
+
+
+// typeerrors
+const b=10;
+console.log(b);
+b=10;
+
+// refernce errors
+console.log(c);
+// c is not defined
