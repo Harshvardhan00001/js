3f797103793947d8e9e3422c11cb1f459ba084bb
Harshvardhan <harshvardhan2672@gmail.com>
2025-09-29 10:07:59 +0530

    hoisting

---
+++ hoisting.js
@@ -0,0 +1,12 @@
+var a ;
+console.log(a); // undefined
+ a = 5;
+console.log(a); // 5
+
+// declaration of function than console at last we declare the variable
+
+// HOISTING CONCEPT
+// THE code not throws error because of hoisting and the code runs without errors
+
+// in hoisting the declaration of variable and function is moved to the top of their scope before code execution
+// only declaration is hoisted not the initialization
